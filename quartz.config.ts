2d18fb21777302ec238789bcbdd78bc4e15daf57
unknown <rajmoham10@gmail.com>
2026-01-04 02:06:31 +0000

    chore: further refinement and customisation

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Digital Garden",
+    pageTitle: "-Digital Garden-",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -64,7 +64,10 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs({
+          rootName: "~",
+          spacerSymbol: " / "
+  }), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
